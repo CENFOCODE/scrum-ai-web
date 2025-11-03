334056361337edffefd87687231f387b2e00a61b
nilcevc <123847308+nilcevc@users.noreply.github.com>
2025-11-03 07:39:27 -0600

    Login, estilos al layout y agrego estilos a styles.scss

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,20 +1,38 @@
 import { CommonModule } from '@angular/common';
-import { Component, ViewChild } from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, signal, ViewChild} from '@angular/core';
 import { FormsModule, NgModel } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
+import { DividerModule } from "primeng/divider";
+import { MatFormFieldModule} from "@angular/material/form-field";
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule} from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterLink],
+    imports: [
+        CommonModule,
+        FormsModule,
+        RouterLink,
+        DividerModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatIconModule,
+        MatButtonModule
+    ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss'],
 })
+
 export class LoginComponent {
   public loginError!: string;
+  private manualDetector: ChangeDetectorRef = inject(ChangeDetectorRef);
   @ViewChild('email') emailModel!: NgModel;
   @ViewChild('password') passwordModel!: NgModel;
+  hide = signal(true);
 
   public loginForm: { email: string; password: string } = {
     email: '',
@@ -22,23 +40,37 @@ export class LoginComponent {
   };
 
   constructor(
-    private router: Router, 
+    private router: Router,
     private authService: AuthService
   ) {}
 
+    clickEvent(event: MouseEvent) {
+        this.hide.set(!this.hide());
+        event.stopPropagation();
+    }
+
+    public clearError() {
+        if (this.loginError) {
+            this.loginError = '';
+            this.manualDetector.markForCheck();
+        }
+    }
+
   public handleLogin(event: Event) {
     event.preventDefault();
-    if (!this.emailModel.valid) {
-      this.emailModel.control.markAsTouched();
-    }
-    if (!this.passwordModel.valid) {
-      this.passwordModel.control.markAsTouched();
-    }
     if (this.emailModel.valid && this.passwordModel.valid) {
       this.authService.login(this.loginForm).subscribe({
-        next: () => this.router.navigateByUrl('/app/dashboard'),
-        error: (err: any) => (this.loginError = err.error.description),
+        next: () => {
+            this.manualDetector.markForCheck();
+            this.router.navigateByUrl('/app/dashboard');
+        },
+        error: (err: any) => {
+            this.manualDetector.markForCheck();
+            this.loginError = err.error.description;
+        },
       });
     }
   }
+
+    protected readonly event = event;
 }
