5b7b786b6fa1677cb28b1f423d46530169a32eeb
drodriguezo18 <126374371+drodriguezo18@users.noreply.github.com>
2025-12-03 04:15:10 -0600

    video room delete

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://trio-effectiveness-verse-carrying.trycloudflare.com',
+  apiUrl: 'http://localhost:8080',
   wsUrl: 'wss://frore-paz-comprehensibly.ngrok-free.dev/webrtc',
   googleClientId: '316836742682-8qljm4bfpqheogsg5eq7v96hl1gn4q65.apps.googleusercontent.com'
 };
